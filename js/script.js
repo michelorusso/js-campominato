06b56f0e4b330a9de5a823a1ccf325f442e10f6e
Michele Lorusso <kirito102095@hotmail.it>
2021-04-06 19:13:21 +0200

    Consegna Esercizio

--- js/script.js
+++ js/script.js
@@ -7,9 +7,11 @@
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
 var arrayBomb = [];
+var totalNumber = 100;
+var bombNumber = 16;
 
-while( arrayBomb.length < 16 ) {
-    var bomb = randomNumberBomb(1 , 100);
+while( arrayBomb.length < bombNumber ) {
+    var bomb = randomNumberBomb(1 , totalNumber);
     console.log(bomb);
 
     // nei numeri bomba non ci dovrann essere duplicati
@@ -23,31 +25,81 @@ console.log(arrayBomb);
 // chiediamo all'utente di inserire (100 - 16) 84 numeri
 // l'utente non può inserire più volte lo stesso numero
 var arrayNumberUser = [];
-while ( arrayNumberUser.length < 84 ) {
+var findBomb = false;
+
+while ( arrayNumberUser.length < (totalNumber - bombNumber) && findBomb == false ) {
+
     var numberUserInput = parseInt( prompt('Dammi un numero') );
 
-    if( !arrayNumberUser.includes(numberUserInput) ) {
-        arrayNumberUser.push(numberUserInput);
-    } else if (arrayNumberUser.includes(numberUserInput) ) {
-        alert('Il numero da te inserito è gia presente, inserire un altro numero');
+
+    
+    var thisNumber = thisNumberBomb( numberUserInput, arrayBomb);
+
+    if( thisNumber == false ) {
+
+        if(!arrayNumberUser.includes(numberUserInput) && numberUserInput < (totalNumber + 1) && numberUserInput > 0 ) {
+
+            arrayNumberUser.push(numberUserInput);
+
+        } else {
+            alert('Il numero è gia presente o non è valido, inserire un altro numero');
+        }
+
+    } else {
+
+        alert('Hai Trovato una Bomba!');
+        findBomb = true;
+
     }
 }
 console.log(arrayNumberUser);
 
-// una variabile che ci indichi se la bomba è stata trovata
-var boomBomb = false;
-
-// uso un ciclo while pre vedere se la bomba è stata trovata
-if(arrayNumberUser.includes(arrayBomb)) {
-    boomBomb = true;
-} 
 
 
-if(boomBomb == true) {
-    alert('hai perso');
-} 
+// Stampiamo i risultati del gioco
+var scoreUser = arrayNumberUser.length;
+console.log(scoreUser);
+
+var result = 'YOU WIN!';
+
+if (findBomb) {
+
+    result = 'YOU LOSE!';
+}
+alert(result);
+
+alert("Your score is : " + scoreUser);
+
 
 // FUNCTION
+
+// funzione che ci da numeri random da 1 a 100
 function randomNumberBomb( min , max ) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
+
+
+// funzione che ci dice se il numero è una bomnba o no
+function thisNumberBomb(numberInput, bombList) {
+
+    // una variabile che ci indichi se la bomba è stata trovata
+    var foundIt = false;
+    var i = 0;
+
+    while( i < bombList.length && foundIt == false) {
+
+        var elementArray = bombList[i];
+
+        if(numberInput == elementArray) {
+
+            foundIt = true;
+
+        }
+
+        i++;
+    }
+
+    return foundIt;
+}
+
+
